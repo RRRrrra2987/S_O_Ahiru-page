acd6ed282996b883e25f51b8f7f525ec3ce9dd96
RRRrrra2987 <77225385+RRRrrra2987@users.noreply.github.com>
2021-04-18 15:44:48 +0900

    Update base.js

--- base.js
+++ base.js
@@ -3,3 +3,14 @@ function sleep(waitMsec) {
     
     while (new Date() - startMsec < waitMsec);
 }
+var count1=1
+function action1(){
+    if (count1%2==1){
+        let target = document.getElementById("reaction1");
+        target.className = "mieru";
+    }
+    else{
+            let target = document.getElementById("reaction1");
+        target.className = "mienai";
+}
+count1=count1+1}
